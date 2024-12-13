519ec70fcbd9b21c5b97887f9def4739aecdade1
madeofsteel1 <maor199979@gmail.com>
2024-12-13 13:47:28 +0200

    modified my_name.java

--- my_name.java
+++ my_name.java
@@ -1,7 +1,7 @@
 public class my_name{
 	public static void main(String[]args){
 			for(int i=1;i<=10;i++){
-					System.out.println("Maor pinhassi"+i);
+					System.out.println("Maor 318801065"+i);
 			}
 	}
 }
